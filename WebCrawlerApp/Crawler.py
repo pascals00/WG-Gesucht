5ccdfdd656db272896db1edbb924b9a21f6a2118
Pascal Schuetze <pascalschutze@gmail.com>
2023-12-08 13:16:50 +0100

    DEV: Crawler extract ads and get information

--- WebCrawlerApp/Crawler.py
+++ WebCrawlerApp/Crawler.py
@@ -4,6 +4,7 @@ from helpers.request.header import *
 from helpers.request.url_manager import *
 from helpers.request.findproxy import *
 from helpers.response.extractUrls import *
+from helpers.response.retrieveRoomInformation import *
 import requests
 
 # -------------------------------------------------------
@@ -15,6 +16,8 @@ requestHeaders = RequestHeaders()
 urlManager = URLManager()
 proxyReader = ProxyReader()
 findproxy = ProxyManager()
+adsExtractor = AdsExtractor()
+infoextractor = HTMLInfoExtractor()
 
 
 districts = suburbFilter.get_all_districts()
@@ -32,15 +35,36 @@ for suburbs in suburbFilter.generate_random_suburb_subsets(testdistrict):
         header_initialized = True
         runningproxie = False
 
-
     while True:
-        page_url = urlManager.switch_page(FilterUrl, currentpage_num)
-        response = requests.get(page_url, proxies=proxies)
         try:
+            page_url = urlManager.switch_page(FilterUrl, currentpage_num)
+            response = requests.get(page_url, proxies=proxies, headers=headers)
             response.raise_for_status()
-            extract_Ads_URL_endings(response.text)
+            adsExtractor.extract_ads_url_endings(response.text)
             currentpage_num += 1
         except requests.exceptions.HTTPError:
+            # HTTP error (e.g., page not found), break the loop
             break
+        except requests.exceptions.RequestException:
+            # Other request errors (e.g., proxy failure), try new proxy
+            proxies = findproxy.find_proxies_FreeProxy()
+            if not proxies:
+                break  # No more proxies available, break the loop
+
+    ad_urls_list = adsExtractor.read_url_endings()
+
+    for ad_url in ad_urls_list:
+        try:
+            response = requests.get(ad_url, proxies=proxies)
+            response.raise_for_status()
+            infoextractor.extract_all
+        except requests.exceptions.HTTPError:
+            # HTTP error (e.g., page not found), break the loop
+            break
+        except requests.exceptions.RequestException:
+            # Other request errors (e.g., proxy failure), try new proxy
+            proxies = findproxy.find_proxies_FreeProxy()
+            if not proxies:
+                break
 
 
