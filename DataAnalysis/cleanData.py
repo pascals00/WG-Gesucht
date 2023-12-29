b22cd690e2ee2e26482490485e3518a5305dab5d
Pascal Schuetze <pascalschutze@gmail.com>
2023-12-29 13:40:33 +0100

    DEV: Clean duplicated in ads url list

--- DataAnalysis/cleanData.py
+++ DataAnalysis/cleanData.py
@@ -8,3 +8,10 @@ df = df[df['apartmentID'].notna()]
 
 # Save the cleaned data to a new CSV file
 df.to_csv('WebCrawlerApp/data/output/apartmentsBerlinData.csv', index=False)
+
+df_url_list = pd.read_csv('WebCrawlerApp/data/output/ADsURLList.csv')
+
+# Remove duplicates from df_url_list
+df_url_list = df_url_list.drop_duplicates()
+
+
